830ae7e6d63ea6b22ed082b3031208cf418c1ecf
Georgi Zlatev <gthebox@gmail.com>
2023-07-10 23:02:46 +0300

    new login file and change login.php, header.php

--- login.php
+++ login.php
@@ -6,10 +6,10 @@
         <h1>Log in</h1>
     <p> No account? <a href="register.php"> Register here!</a></p>
 
-    <form action="" method="post">
+    <form action="includes/login-inc.php" method="post">
         <input type="text" name="username" placeholder="Username">   <input type="password" name="password" placeholder="Password">
 <!--        <input type="email" name="email" placeholder="Email">-->
-        <button type="submit">Login</button>
+        <button type="submit" name="submit">Login</button>
     </form>
 </div>
 
